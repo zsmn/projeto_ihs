89a75b71ec8278dbc9f73a0d4cbacb297865696e
DE2i-150 <de2i-150@de2i150-Cedar-Trail-Client-platform.(none)>
2019-11-26 17:47:15 -0300

    versao final

---
+++ driver/.altera_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/de2i-150/Desktop/projetoihs/driver/altera_driver.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.13.0-32-generic/scripts/module-common.lds --build-id  -o /home/de2i-150/Desktop/projetoihs/driver/altera_driver.ko /home/de2i-150/Desktop/projetoihs/driver/altera_driver.o /home/de2i-150/Desktop/projetoihs/driver/altera_driver.mod.o
---
+++ driver/.tmp_versions/altera_driver.mod
@@ -0,0 +1,2 @@
+/home/de2i-150/Desktop/projetoihs/driver/altera_driver.ko
+/home/de2i-150/Desktop/projetoihs/driver/altera_driver.o
